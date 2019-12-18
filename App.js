0e76b7ef6b7d0effc8a21b3b8fcc4cb70b884987
Pauline Drouhin <pauline.drouhin@gmail.com>
2019-12-18 19:34:13 +0100

    test

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button, Alert, Text } from 'react-native';
+
+// navigation //
+// import { TabNavigator } from 'react-navigation';
+// import TabOne from './TabOne';
+// import TabTwo from './TabTwo';
+
+// export default TabNavigator({
+//     TabOne: { screen: TabOne },
+//     TabTwo: { screen: TabTwo },
+// });
 
 export default class App extends React.Component {
+
+  handleClick() {
+    Alert.alert('Hello !');
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Pauline</Text>
-       
+        <Text> Click on the button BITCH : </Text>
+        <Button onPress={this.handleClick.bind(this)} title="Go" />
+        <TabOne/>
       </View>
     );
   }
@@ -17,6 +33,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop:"20%"
   },
 });
