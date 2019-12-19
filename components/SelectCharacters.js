4d0bb1e51cd50c141f19c360e53e1e167d370ff9
Fanny <fanny.bouffier@gmail.com>
2019-12-19 23:07:52 +0100

    fix(selectCharacters):fixed issues with layout and fonts

--- components/SelectCharacters.js
+++ components/SelectCharacters.js
@@ -4,7 +4,7 @@ import octopus from "../assets/pictures/card-octopus.jpg";
 import unicorn from "../assets/pictures/card-unicorn.jpg";
 import cat from "../assets/pictures/card-cat.jpg";
 import background from "../assets/pictures/background.jpg"
-import { AppLoading } from 'expo';
+import { AppLoading, AuthSession } from 'expo';
 import * as Font from 'expo-font';
 
 
@@ -56,7 +56,7 @@ export default function SelectCharacter ({ history }) {
         <View style={styles.playerOne}>
           <Text style={styles.text2}> PLAYER 1</Text>
           <View style={styles.characters}>
-            <View>
+            <View  style={styles.intern}>
               <Text
                 name="octopus"
                 onPress={() =>
@@ -66,23 +66,24 @@ export default function SelectCharacter ({ history }) {
                 <Image
                   name="octopus"
                   source={octopus}
-                  style={{ width: 100, height: 100 }}
+                  style={{  height: 100, width: 100 }}
                 />
               </Text>
             </View>
-            <View>
+            <View style={styles.intern}>
               <Text
                 name="cat"
                 onPress={() => setPlayerOne({ name: "cat", picture: { cat } })}
               >
                 <Image
                   name="cat"
-                  style={{ width: 100, height: 100 }}
+                  style={{  height: 100, width: 100 }}
+                  resizeMode="contain"
                   source={cat}
                 />
               </Text>
             </View>
-            <View>
+            <View style={styles.intern}>
               <Text
                 name="unicorn"
                 onPress={() =>
@@ -91,7 +92,7 @@ export default function SelectCharacter ({ history }) {
               >
                 <Image name="unicorn"
                 source={unicorn}
-                style={{ width: 100, height: 100 }}/>
+                style={{  height: 100, width: 100 }} resizeMode="contain"/>
 
               </Text>
             </View>
@@ -101,8 +102,8 @@ export default function SelectCharacter ({ history }) {
         <View style={styles.playerTwo}>
           <Text style={styles.text2}> PLAYER 2</Text>
           <View style={styles.characters}>
-            <View>
-              <Text
+            <View style={styles.intern}>
+              <Text 
                 name="octopus"
                 onPress={() =>
                   setPlayerTwo({ name: "octopus", picture: { octopus } })
@@ -116,19 +117,19 @@ export default function SelectCharacter ({ history }) {
               </Text>
             </View>
 
-            <View>
+            <View style={styles.intern}>
               <Text
                 name="cat"
                 onPress={() => setPlayerTwo({ name: "cat", picture: { cat } })}
               >
                 <Image
                   name="cat"
-                  style={{ width: 100, height: 100 }}
+                  style={{ width: 100, height: 100 , top:0}}
                   source={cat}
                 />
               </Text>
             </View>
-            <View>
+            <View style={styles.intern}>
               <Text
                 name="unicorn"
                 onPress={() =>
@@ -139,14 +140,16 @@ export default function SelectCharacter ({ history }) {
                 source={unicorn}
                 name="unicorn"
                 style={{ width: 100, height: 100 }}/>
-                
+
               </Text>
             </View>
           </View>
-          <Text style={styles.buttonCheck} title="Valide" onPress={() => checkPick()}>
+          
+        </View>
+        <Text style={styles.buttonCheck} title="Valide" onPress={() => checkPick()}>
                 FIGHT !
           </Text>
-        </View>
+
       </View>
     </View>
     </ImageBackground>
@@ -158,7 +161,7 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 50,
     fontFamily: "Londrina",
-    top: 70,
+    top: 50,
     padding:7,
     paddingLeft:15,
     paddingRight:15,
@@ -170,6 +173,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontFamily: "Londrina",
     color:"rgb(122, 53, 209)",
+    marginBottom:50,
   },
   text2: {
     fontSize: 30,
@@ -180,29 +184,36 @@ const styles = StyleSheet.create({
   container: {
     maxHeight: "100%",
     flex: 1,
-    alignItems: "center"
+    alignItems: "center",
   },
   characters: {
-    maxHeight: 140,
     width: 350,
+    height: 400,
     flex: 1,
     flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-around"
+    alignItems: "flex-start",
+    justifyContent: "space-around",
+    backgroundColor:"green",
   },
+  intern : {
+    backgroundColor:"blue",
+  },
+
   playerOne: {
     maxHeight: 200,
     flex: 1,
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    backgroundColor:"pink"
   },
   playerTwo: {
     maxHeight: 200,
     flex: 1,
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    backgroundColor:"red"
   },
   playersBox: {
     flex: 1,
