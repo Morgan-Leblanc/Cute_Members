73034098c31775ccc5df3018a12092c1a1fda98b
Pauline Drouhin <pauline.drouhin@gmail.com>
2019-12-19 12:23:45 +0100

    starting to code simon game
    Co-authored-by: Flauz <dread94@gmail.com>

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NativeRouter, Switch, Route } from "react-router-native";
+import Simon from "./components/Simon"
+export default class App extends React.Component {
+  render() {
+    return (
+      <NativeRouter>
+        <View style={styles.container}>
+          <Switch>
+            {/* <Route exact path="/" component={Home} />
+            <Route exact path="/newgame" component={Morguy} /> */}
+            <Route exact path="/" component={Simon} />
+
+
+          </Switch>
+        </View>
+      </NativeRouter>
+    );
+  }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
-
-
-
---
+++ components/Simon.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { StyleSheet, View, Text, Button, TouchableOpacity } from "react-native";
+import { AuthSession } from "expo";
+import { TouchableHighlight } from "react-native-gesture-handler";
+
+const styles = StyleSheet.create({
+    player: {
+        fontSize: 60,
+        textAlign: 'center',
+        bottom: 50,
+        fontWeight: 'bold'
+    },
+    button: {
+        fontSize: 60,
+        color: "white",
+        backgroundColor: "black",
+        top: 70,
+        paddingHorizontal: 20,
+        borderRadius: 20
+    },
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    colorTop: {
+        flexDirection: 'row',
+
+    },
+    colorBottom: {
+        flexDirection: 'row'
+    },
+    blue: {
+        padding: 90,
+        margin: 5,
+        backgroundColor: "blue"
+    },
+    yellow: {
+        padding: 90,
+        margin: 5,
+
+        backgroundColor: "yellow"
+    }, red: {
+        padding: 90,
+        margin: 5,
+
+        backgroundColor: "red"
+    },
+    green: {
+        padding: 90,
+        margin: 5,
+        backgroundColor: "green"
+    },
+    green: active {
+        padding: 90,
+        margin: 5,
+        backgroundColor: "green"
+    },
+    challenge: {
+        fontSize: 40
+    }
+
+
+})
+
+export default ({ history }) => (
+    <View style={styles.container} >
+        <Text style={styles.player}>Player ?</Text>
+        <Text style={styles.challenge}>This is your challenge :</Text>
+        <View style={styles.colorTop}>
+            <TouchableHighlight>
+            <View style={styles.blue} >
+            </View>
+            </TouchableHighlight>
+            <View style={styles.red}>
+            </View>
+        </View>
+        <View style={styles.colorBottom}>
+            <View style={styles.yellow}>
+            </View>
+            <View style={styles.green}>
+            </View>
+        </View>
+    </View>
+
+
+);
