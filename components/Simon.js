c4ef9575843f988903885b75fdbe9b989cb44a29
hikkary <hikkary@MacBook-Pro-de-Zakaria.local>
2019-12-20 03:48:30 +0100

    feat(simon): in progress

--- components/Simon.js
+++ components/Simon.js
@@ -1,236 +1,159 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Text, Button, TouchableOpacity } from "react-native";
-import { TouchableHighlight } from "react-native-gesture-handler";
+import {  View, Text } from "react-native";
 import styled from 'styled-components'
 
 
+const getRandomInt = (max)  =>{
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+const allColors = ['red', 'blue', 'green', 'yellow']
+
+const sequenceInit = [2, 1, 4]
+
+const verifySequence = ({value, sequence, indexInSequence, setIndexInSequence, setMode ,addToSequence,activateSquare}) => {
+    if(sequence[indexInSequence] === value) {
+        if(!sequence[indexInSequence + 1]){
+            console.log('www') //victoire
+            addToSequence([ ...sequence, getRandomInt(4)])
+            setIndexInSequence(0)
+            setMode('listen')
+            playSequence(sequence,activateSquare,setMode)
+        }
+       
+        setIndexInSequence(indexInSequence + 1)
+
+        return true
+    }
+    else {
+        //rater
+        setIndexInSequence(0)
+        setMode('idle')
+        return false
+    }
+}
+
+const playSequence = async (sequence, activateSquare, setMode) => {
+    const intervalTime = 800
+    let delta
+    setMode('listen')
+    const test = sequence.map((seq, index) => {
+         delta = index + 1
+        setTimeout(() => {activateSquare(seq)}, intervalTime * delta)
+    })
+    if(test){
+    delta = delta+1
+    setTimeout(() => {setMode('play')}, intervalTime * delta )
+}
+}
 
 export default ({ history }) => {
-// state //
-    const [color, setColor] = useState({
-        color : null
-    });
-    const [yellowLighter, setYellowLighter] = useState({
-        boolean :false,
-        color : "yellow"
-    });;
-    const [greenLighter, setGreenLighter] = useState({
-        boolean :false,
-        color : "green"
-    });;
-    const [redLighter, setRedLighter] = useState({
-        boolean :false,
-        color : "red"
-    });;
+    // const [color, setColor] = useState({
+    //     color : null
+    // });
+    // useEffect( () => {
+    //     setColor('blue')
+    //     const timer = setTimeout(() => setColor(null), 1000)
+    //     return () => {
+    //         clearTimeout(timer)
+    //     }
+    // }, [])
+    const [currentSquare, activateSquare] = useState(0)
 
-    const [colors, setColors] = useState(['blue', 'red', 'yellow', 'green'])
-    const [tab1, setTab1] = useState([])
+    useEffect(() =>{ 
+        console.log(currentSquare)
+    })
 
-    useEffect( () => {
-        console.log('useEffect')
-        setColor('blue')
-        const timer = setTimeout(() => setColor(null), 1000)
-        return () => {
-            clearTimeout(timer)
-        }
-    }, [])
+    const [sequence, addToSequence] = useState(sequenceInit)
+
+    useEffect(() =>{ 
+        console.log(sequence)
+
+    },[sequence])
+
+    const [mode, setMode] = useState('idle')
+    useEffect(() => {
+        console.log(mode)
+        activateSquare(0)
+        
+    },[mode] )
+
+    const [indexInSequence, setIndexInSequence] = useState(0)
+    useEffect(() => {
+        console.log(indexInSequence)
+        activateSquare(0)
+        
+    },[indexInSequence] )
 
 // Algo pour générer un tableau aléatoire // 
-// ici on récupère à la place de i < x le montant du slider passé en props //
-fillTab1 = () => {
-    const tabTmp = []
-    for (i = 0; i < 6; i++) {
-        const index = Math.floor(Math.random() * colors.length)
-        tabTmp.push(colors[index])
-        // console.log("tab 1 final", tab1)
-        // console.log("colors index", colors[index])
-    }
-    setTab1(tabTmp)
-}
-
+// ici on récupère à la place de i < x le montant du slider passé
+//  en props //
 
 // Algo pour jouer les couleurs du tableau aléatoire //
-colorsLighter =  ()  => {
-    tab1.map(color => {
-        console.log('bonjour',blueLighter.color , color)
-        setBlueLighter({...blueLighter, boolean: !blueLighter.boolean});
-        
-        if(blueLighter.color == color){
-            let i = 0
-            console.log("ici");
-            // setInterval(()=> {
-                // i++
-                // if(true){
-                    while(i < 2)
-                    {
-                        
-                        setBlueLighter({...blueLighter, boolean: !blueLighter.boolean})
-                        i++
-                    }
-                    // console.log(blueLighter.boolean)
-                    console.log("ici 1");
-                    // }
-                    // }, 1000);
-                    // clearInterval(x)
-                    // setBlueLighter({...blueLighter, boolean: false})
-                    
-                }
-            } )
-         
-}
-
-   // console.log(i,blueLighter );
-            // tab1.map(async color => {
-                //     console.log('elemennt color',color)
-                //     if (color === blueLighter.color) {
-                    //         console.log();
-            
-    //         await setTimeout(() => setBlueLighter(blueLighter.boolean = !blueLighter.boolean),2000)
-    //         console.log('state blue',blueLighter.boolean)
-    //         setTimeout(() => setBlueLighter(blueLighter.boolean = !blueLighter.boolean),2000)
-    //         console.log('state blue',blueLighter.boolean)
-
-    //     }
-    //     else if (color === yellowLighter.color) {
-    //         await setTimeout(() => setYellowLighter(yellowLighter.boolean = !yellowLighter.boolean),2000)
-    //         console.log('state blue',yellowLighter.boolean)
-
-    //         setTimeout(() => setYellowLighter(yellowLighter.boolean = !yellowLighter.boolean),2000)
-    //         console.log('state blue',yellowLighter.boolean)
-
-    //     }
-    //     else if (color === greenLighter.color) {
-    //         await setTimeout(() => setGreenLighter(greenLighter.boolean = !greenLighter.boolean),2000)
-    //         console.log('state blue',greenLighter.boolean)
-    //         setTimeout(() => setGreenLighter(greenLighter.boolean = !greenLighter.boolean),2000)   
-    //         console.log('state blue',greenLighter.boolean)
-    //       }
-    //     else if (color === redLighter.color) {
-    //         await setTimeout(() => setRedLighter(redLighter.boolean = !redLighter.boolean),2000)
-    //         console.log('state blue',redLighter.boolean)
-    //         setTimeout(() => setRedLighter(redLighter.boolean = !redLighter.boolean),2000)   
-    //         console.log('state blue',redLighter.boolean)
-    //        }
-    // })
-    
-
- console.log('salut',tab1)
+    const displayMessage = (value) => {
+        switch (value) {
+            case 'play':
+                return 'A Toi de jouer !'
+            case 'listen':
+                return 'Regarde La Sequence'
+            case 'idle':
+                return 'Appui sur commencer pour commencer'
+            default:
+                break;
+        }
+    }
 
     return (
-        <View style={styles.container} >
-            <Text style={styles.player}>Player ?</Text>
+        <View  >
+            <Text >Player ?</Text>
             <Text onPress={() => fillTab1()}>Remplir tableau aléatoire</Text>
-            {/* <Text > bool {blueLighter.boolean.toString()}</Text> */}
-            <Text style={styles.challenge}>This is your challenge :</Text>
-            <View style={styles.colorTop}>
-                <Square color={color} />
-                {/* <View style={blueLighter.boolean ? styles.blueLight : styles.blue} > */}
-                {/* <View style={} >
-
-                </View> */}
-
-                {/* <View style={redLighter.boolean ? styles.redLight : styles.red}> */}
-                {/* </View> */}
-            {/* </View> */}
-            {/* <View style={styles.colorBottom}> */}
-                {/* <View style={yellowLighter.boolean ? styles.yellowLight : styles.yellow}> */}
-                {/* </View> */}
-                {/* <View style={greenLighter.boolean ? styles.greenLight : styles.green}> */}
-                {/* </View> */}
-            </View>
-
-
-
-            <Text onPress={colorsLighter}>Allumer le tableau</Text>
-
-
+            <Text>This is your challenge :</Text>
+            <Text>Niveau : {sequence.length - 2}</Text>
+            <Text>{displayMessage(mode)}</Text>
+            <SquareContainer >
+            <SquareRow>
+                <Square onPress={mode ==="play" ? () => verifySequence({value:1,sequence, indexInSequence, setIndexInSequence, setMode,addToSequence, activateSquare}) : null} color={allColors[0]} isActivated={currentSquare === 1} />
+                <Square onPress={mode ==="play" ? () => verifySequence({value:2,sequence, indexInSequence, setIndexInSequence, setMode,addToSequence, activateSquare}) : null}  color={allColors[1]} isActivated={currentSquare === 2}/>
+                </SquareRow>
+                <SquareRow>
+                <Square onPress={mode ==="play" ? () => verifySequence({value:3,sequence, indexInSequence, setIndexInSequence, setMode,addToSequence, activateSquare}) : null}  color={allColors[2]} isActivated={currentSquare === 3}/>
+                <Square onPress={mode ==="play" ? () => verifySequence({value:4,sequence, indexInSequence, setIndexInSequence, setMode,addToSequence, activateSquare}) : null}  color={allColors[3]} isActivated={currentSquare === 4}/>
+                </SquareRow>
+            </SquareContainer>
+            <StartButton onPress={() => playSequence(sequence, activateSquare,setMode)}>
+            <CommonTextButton>Allumer le tableau</CommonTextButton>
+            </StartButton>
         </View >
     )
-
-
-
 };
 
-const Square = styled.div`
-    background-color: ${props => (props => (props.color ? props.color : 'rgba(72,238,255, 1)'))};
-    padding: 90;
-    margin: 5;
+const SquareRow = styled.View`
+flex-direction:row;
+flex:1;
+
 `
 
-// const styles = StyleSheet.create({
+const SquareContainer = styled.View`
+flex-wrap: wrap;
+flex:1;
+max-height: 50%;
+`
 
+const CommonTextButton = styled.Text`
+    font-size: 24px;
+    color: white
+`
 
-//     player: {
-//         fontSize: 60,
-//         textAlign: 'center',
-//         bottom: 50,
-//         fontWeight: 'bold'
-//     },
-//     button: {
-//         fontSize: 60,
-//         color: "white",
-//         backgroundColor: "black",
-//         top: 70,
-//         paddingHorizontal: 20,
-//         borderRadius: 20
-//     },
-//     container: {
-//         justifyContent: 'center',
-//         alignItems: 'center'
-//     },
-//     colorTop: {
-//         flexDirection: 'row',
+const StartButton = styled.TouchableOpacity`
+    padding:12px;
+    background-color: #f5a4b0
+    border-radius: 12px;
+`
 
-//     },
-//     colorBottom: {
-//         flexDirection: 'row'
-//     },
-//     blue: {
-//         padding: 90,
-//         margin: 5,
-//         backgroundColor: props => (styles.blue : 'rgba(72,238,255, 1)'
-//     },
-//     blueLight: {
-//         padding: 90,
-//         margin: 5,
-//         backgroundColor: 'rgba(255,207,55, 1)'
+const Square = styled.TouchableOpacity`
+    background-color: ${props => props.isActivated ? props.color : 'grey'};
+    margin: 3px;
+    min-width: 150px;
 
-//         // backgroundColor: 'rgba(72,238,255,0.5)'
-//     },
-
-//     yellow: {
-//         padding: 90,
-//         margin: 5,
-//         backgroundColor: 'rgba(255,207,55, 1)'
-//     },
-//     yellowLight: {
-//         padding: 90,
-//         margin: 5,
-//         backgroundColor: 'rgba(255,207,55, 0.5)'
-//     },
-
-//     red: {
-//         padding: 90,
-//         margin: 5,
-//         backgroundColor: 'rgba(250,73,110, 1)'
-//     },
-//     redLight: {
-//         padding: 90,
-//         margin: 5,
-//         backgroundColor: 'rgba(250,73,110, 0.5)'
-//     },
-
-//     green: {
-//         padding: 90,
-//         margin: 5,
-//         backgroundColor: 'rgba(122,53,209, 1)'
-//     },
-//     greenLight: {
-//         padding: 90,
-//         margin: 5,
-//         backgroundColor: 'rgba(122,53,209, 0.5)'
-//     },
-//     challenge: {
-//         fontSize: 40
-//     }
-// })
+    min-height: 50px;
+`
